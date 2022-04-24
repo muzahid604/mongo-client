ed9d6e6463b367d84033abe52a497cc44d772d10
muzahid604 <604muzahid@gmail.com>
2022-04-25 02:20:44 +0600

    server data founded

--- src/componetnt/AddUser/AddUser.js
+++ src/componetnt/AddUser/AddUser.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
 const AddUser = () => {
+    const handleSubmit = event => {
+        event.preventDefault()
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = { name, email }
+
+        //send data in server site
+        fetch('http://localhost:5000/user', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
     return (
         <div>
-            this is add user
+            <h1>please add </h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='name' required placeholder='Enter your name' />
+                <br />
+                <input type="email" name='email' required placeholder='Enter your Email' />
+                <br />
+                <input type="submit" value="add this" />
+            </form>
         </div>
     );
 };
