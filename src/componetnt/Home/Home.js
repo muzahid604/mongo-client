246b98d60b72f5cfc76de459f45278b03ef3b661
muzahid604 <604muzahid@gmail.com>
2022-04-25 12:15:08 +0600

    set users successfully

--- src/componetnt/Home/Home.js
+++ src/componetnt/Home/Home.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
     return (
         <div>
-            this is home
+            our users:{users.length}
+            <h1>user name or email</h1>
+            <br />
+            {
+                users.map(user => <li>{user.name}:::::{user._id}:::::{user.email}</li>)
+            }
         </div>
     );
 };
